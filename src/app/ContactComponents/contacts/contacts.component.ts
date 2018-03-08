4600d7b982663dd39859ed207ec92c4b513db7e0
Andrew Morris <drew.mmorris@gmail.com>
2018-03-08 11:43:45 -0700

    Moving contact page landing

--- src/app/ContactComponents/contacts/contacts.component.ts
+++ src/app/ContactComponents/contacts/contacts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from '../../contact';
-import { CONTACTS } from '../../mock-contacts';
+import { Contact } from '../../../contact';
+import { CONTACTS } from '../../../mock-contacts';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
