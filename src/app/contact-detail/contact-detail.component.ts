03e90e6fffc726fd61e1eae81916d7c612e9c990
Andrew Morris <drew.mmorris@gmail.com>
2018-03-08 17:04:56 -0700

    updates

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Contact } from '../contact';
+import { Contact } from '../../contact';
 
 @Component({
   selector: 'app-contact-detail',
--- src/contact.ts
+++ src/contact.ts
@@ -1,5 +1,3 @@
-
-
 export class Contact {
     title: string;
     name: string;
