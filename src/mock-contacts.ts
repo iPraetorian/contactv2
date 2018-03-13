867a71fcaf0c944f8b0c3ad464dc070d5f248afb
Andrew Morris <drew.mmorris@gmail.com>
2018-03-13 11:28:22 -0600

    updated images

--- src/mock-contacts.ts
+++ src/mock-contacts.ts
@@ -2,13 +2,13 @@ import { Contact } from './contact';
 
 export const CONTACTS: Contact[] = [
     {id: 1, title: 'Climber', name: 'Royal Robbins', image: 'assets/images/rRobins.jpg'},
-    {id: 2, title: 'Climber', name: 'Jim Bridwell', image: ''},
-    {id: 3, title: 'Climber', name: 'Dale Bard', image: ''},
-    {id: 4, title: 'MotoGP Racer', name: 'Nicky Hayden', image: ''},
-    {id: 5, title: 'MotoGP Racer', name: 'Max Biaggi', image: ''},
-    {id: 6, title: 'MotoGP Racer', name: 'Marco Simoncelli', image: ''},
-    {id: 7, title: 'Painter', name: 'Kara Walker', image: ''},
-    {id: 8, title: 'Painter', name: 'Jean-Michael Basquiat', image: ''},
-    {id: 9, title: 'Writer', name: 'Jack Kerouac', image: ''},
-    {id: 10, title: 'Writer', name: 'Richard Wright', image: ''}
+    {id: 2, title: 'Climber', name: 'Jim Bridwell', image: 'assets/images/jBridwell.jpg'},
+    {id: 3, title: 'Climber', name: 'Dale Bard', image: 'assets/images/dBard.jpg'},
+    {id: 4, title: 'MotoGP Racer', name: 'Nicky Hayden', image: 'assets/images/nHayden.jpg'},
+    {id: 5, title: 'MotoGP Racer', name: 'Max Biaggi', image: 'assets/images/mBiaggi.jpg'},
+    {id: 6, title: 'MotoGP Racer', name: 'Marco Simoncelli', image: 'assets/images/mSimoncelli.jpg'},
+    {id: 7, title: 'Painter', name: 'Kara Walker', image: 'assets/images/kWalker.jpg'},
+    {id: 8, title: 'Painter', name: 'Jean-Michael Basquiat', image: 'assets/images/jmBasquiat.jpg'},
+    {id: 9, title: 'Writer', name: 'Jack Kerouac', image: 'assets/images/jKerouac.jpg'},
+    {id: 10, title: 'Writer', name: 'Richard Wright', image: 'assets/images/rWright.jpg'}
 ];
